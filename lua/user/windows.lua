f610725af62827853cb94b8fef8da65e2b4c186f
Maya Rose <maya@mayarose.io>
2026-08-12 19:32:32 -0300

    Update vscode on Windows.

--- lua/user/windows.lua
+++ lua/user/windows.lua
@@ -1,7 +1,7 @@
 vim.g.csharpls_path = "C:\\Users\\maya\\.dotnet\\tools\\csharp-ls.EXE"
 vim.g.netcoredbg = "C:\\Users\\maya\\scoop\\shims\\netcoredbg.EXE"
 
-vim.g.signer_path = vim.env.HOME .. "/AppData/Local/Programs/Microsoft VS Code/df53daabb1/resources/app/node_modules.asar.unpacked/vsda/build/Release/vsda.node"
+vim.g.signer_path = vim.env.HOME .. "/AppData/Local/Programs/Microsoft VS Code/a5b5009513/resources/app/node_modules.asar.unpacked/vsda/build/Release/vsda.node"
 vim.g.debugger_path = vim.env.HOME .. "/.vscode/extensions/ms-dotnettools.csharp-2.148.23-win32-x64/.debugger/x86_64/vsdbg-ui.exe"
 
 vim.keymap.set('n', '<leader>t', '<cmd>sp | term pwsh -nol<cr>', {silent=true})
