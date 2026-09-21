8e28062df2dfde721b15636e439e1f0ab6a459f2
Maya Rose <maya@mayarose.io>
2026-09-21 14:58:56 -0300

    Updates for VSCode - Linux.

--- lua/user/unix.lua
+++ lua/user/unix.lua
@@ -2,7 +2,7 @@ vim.g.csharpls_path = "/usr/sbin/csharp-ls"
 vim.g.netcoredbg = "/usr/sbin/netcoredbg"
 
 vim.g.signer_path = "/usr/share/code/resources/app/node_modules.asar.unpacked/vsda/build/Release/vsda.node"
-vim.g.debugger_path = vim.env.HOME .. "/.vscode/extensions/ms-dotnettools.csharp-2.140.9-linux-x64/.debugger/vsdbg-ui"
+vim.g.debugger_path = vim.env.HOME .. "/.vscode/extensions/ms-dotnettools.csharp-2.160.4-linux-x64/.debugger/vsdbg-ui"
 vim.g.cppdbg_path = vim.env.HOME .. "/.vscode/extensions/ms-vscode.cpptools-1.34.4-linux-x64/debugAdapters/bin/OpenDebugAD7"
 vim.g.gdb_path = "/usr/bin/gdb"
 
