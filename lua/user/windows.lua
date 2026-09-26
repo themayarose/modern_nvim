553b51590429ddfca37fcc0c02476d8e7e5e02b5
Maya Rose <maya@mayarose.io>
2026-09-26 14:44:48 -0300

    VSCode for Windows updated.

--- lua/user/windows.lua
+++ lua/user/windows.lua
@@ -1,7 +1,7 @@
 vim.g.csharpls_path = "C:\\Users\\maya\\.dotnet\\tools\\csharp-ls.EXE"
 vim.g.netcoredbg = "C:\\Users\\maya\\scoop\\shims\\netcoredbg.EXE"
 
-vim.g.signer_path = vim.env.HOME .. "/AppData/Local/Programs/Microsoft VS Code/2242ebbb54/resources/app/node_modules.asar.unpacked/vsda/build/Release/vsda.node"
+vim.g.signer_path = vim.env.HOME .. "/AppData/Local/Programs/Microsoft VS Code/04c0d99f4f/resources/app/node_modules.asar.unpacked/vsda/build/Release/vsda.node"
 vim.g.debugger_path = vim.env.HOME .. "/.vscode/extensions/ms-dotnettools.csharp-2.160.4-win32-x64/.debugger/x86_64/vsdbg-ui.exe"
 vim.g.cppdbg_path = vim.env.HOME .. "/.vscode/extensions/ms-vscode.cpptools-1.35.2-win32-x64/debugAdapters/bin/OpenDebugAD7.exe"
 vim.g.gdb_path = vim.env.HOME .. "/scoop/shims/gdb.exe"
